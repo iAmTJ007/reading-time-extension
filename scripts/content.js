74fb9ddbd150bf6d974350f16e149d7f146886fb
Tathagya <eveninghappy11@gmail.com>
2026-01-15 22:54:43 +0530

    works on al jazeera, hindustan times

--- scripts/content.js
+++ scripts/content.js
@@ -1,51 +1,3 @@
-// function renderReadingTime(article) {
-//   // If we weren't provided an article, we don't need to render anything.
-//   if (!article) {
-//     return;
-//   }
-
-//   const text = article.textContent;
-//   const wordMatchRegExp = /[^\s]+/g; // Regular expression
-//   const words = text.matchAll(wordMatchRegExp);
-//   // matchAll returns an iterator, convert to array to get word count
-//   const wordCount = [...words].length;
-//   const readingTime = Math.round(wordCount / 200);
-//   const badge = document.createElement("p");
-//   // Use the same styling as the publish information in an article's header
-//   badge.classList.add("color-secondary-text", "type--caption");
-//   badge.textContent = `⏱️ ${readingTime} min read`;
-
-//   // Support for API reference docs
-//   const heading = article.querySelector("h1");
-//   // Support for article docs with date
-//   const date = article.querySelector("time")?.parentNode;
-
-//   (date ?? heading).insertAdjacentElement("afterend", badge);
-// }
-
-// renderReadingTime(document.querySelector("article"));
-
-
-// const observer = new MutationObserver((mutations) => {
-//   for (const mutation of mutations) {
-//     // If a new article was added.
-//     for (const node of mutation.addedNodes) {
-//       if (node instanceof Element && node.tagName === 'ARTICLE') {
-//         // Render the reading time for this particular article.
-//         renderReadingTime(node);
-//       }
-//     }
-//   }
-// });
-
-// https://developer.chrome.com/ is a SPA (Single Page Application) so can
-// update the address bar and render new content without reloading. Our content
-// script won't be reinjected when this happens, so we need to watch for
-// changes to the content.
-// observer.observe(document.querySelector('devsite-content'), {
-//   childList: true
-// });
-
 function renderReadingTime(article){
     if(!article)return;
     //first we extract text from the article
@@ -60,10 +12,15 @@ function renderReadingTime(article){
     console.log(words);
     console.log(words.length);
     //now calculate reading time
-    const readingTime=words.length/200;
+    const readingTime=Math.ceil(words.length/200);
+    //create new element badge and attach it after h1 tag in article
     const badge=document.createElement("p");
     badge.textContent=`⏱️ ${readingTime} min read`;
-    article.querySelector("h1").appendChild(badge);
+    document.querySelector("h1").after(badge);
 }
-
-renderReadingTime(document.querySelector("article"))
+function fetchArticle(){
+    const article=document.querySelector("article")||document.querySelector("articlePage")||document.querySelector("main")||document.body;
+    console.log("fetching article");
+    return article;
+}
+renderReadingTime(fetchArticle())
